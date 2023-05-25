8e519e1517d41c11ec76d1ad466db07a7578de59
Andrii Mendela <andrii.mendela@gmail.com>
2023-05-25 14:59:18 +0300

    added count of print

--- qr_catridges.py
+++ qr_catridges.py
@@ -18,10 +18,10 @@ def select_type():
 
 def gen_qr(data):
     qr = qrcode.QRCode(
-        version=5,
-        error_correction=qrcode.constants.ERROR_CORRECT_Q,
-        box_size=7,
-        border=1,
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        #box_size=7,
+        border=3,
     )
 
     qr.add_data(data)
@@ -32,8 +32,9 @@ def gen_qr(data):
 
 def print_btn():
     value = form_data()
+    copies = copy_ent.get()
     gen_qr(value)
-    subprocess.run(["lp", "-d", "RT200", '-o', 'fit-to-page', 'qr_code.png'])
+    subprocess.run(["lp", "-d", "RT200", '-o', 'fit-to-page', f'-n{copies}', 'qr_code.png'])
     if os.path.exists("qr_code.png"):
         os.remove("qr_code.png")
 
@@ -44,28 +45,36 @@ root.title('Qrcode Print')
 #scr_width = int(root.winfo_screenwidth()/2)
 scr_height = int(root.winfo_screenheight()/2)
 
-root.geometry(f'325x130+{540}+{scr_height}')
+root.geometry(f'305x145+{540}+{scr_height}')
 root.resizable(False, False)
 
-lb_id = Label(root, text='Enter --> id ')
-lb_id.grid(row=0, column=0, padx=10, pady=10)
+main_frame = LabelFrame(root, text='Main settings')
+main_frame.grid(row=0, column=0, padx=5, pady=5)
 
-ent_id = Entry(root, width=15)
-ent_id.grid(row=0, column=1, padx=10, pady=10)
+lb_id = Label(main_frame, text='Enter --> id ')
+lb_id.grid(row=0, column=0, padx=3, pady=3)
+
+ent_id = Entry(main_frame, width=15)
+ent_id.grid(row=0, column=1, padx=3, pady=3)
+
+copy_id = Label(main_frame, text='Copies')
+copy_id.grid(row=1, column=0, padx=3, pady=3, sticky='w')
+
+copy_ent = Entry(main_frame, width=15)
+copy_ent.grid(row=1, column=1, padx=3, pady=3)
+
+copy_ent.insert(0, '1')
 
 val = StringVar()
 val.set('printer')
 
-rb_cartridge = Radiobutton(root, text='Printer', variable=val, value='printer', command=select_type)
-rb_cartridge.grid(row=1, column=0, padx=5, pady=5, sticky='w')
+rb_cartridge = Radiobutton(main_frame, text='Printer', variable=val, value='printer', command=select_type)
+rb_cartridge.grid(row=2, column=0, padx=3, pady=3, sticky='w')
 
-rb_printer = Radiobutton(root, text='Computer', variable=val, value='computer', command=select_type)
-rb_printer.grid(row=1, column=1, padx=5, pady=5, sticky='w')
+rb_printer = Radiobutton(main_frame, text='Computer', variable=val, value='computer', command=select_type)
+rb_printer.grid(row=2, column=1, padx=3, pady=3, sticky='w')
 
-print_btn = Button(root, text='Print', command=print_btn)
+print_btn = Button(main_frame, text='Print', command=print_btn)
 print_btn.grid(row=2, columnspan=2, padx=5, pady=5, sticky='we')
 
 root.mainloop()
-
-
-
